364c1c4f12769a6175e86f47cf000a2616d2845c
Justin Lu <Justin_Lu@student.uml.edu>
2021-03-29 16:33:37 -0400

    Added tilesets, gravity to the movement

--- script/Player.gd
+++ script/Player.gd
@@ -1,26 +1,27 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var SPEED = 200
+export (int) var G = 1000
 
 var velocity = Vector2()
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 func get_input():
 	velocity.x = 0
 	if(Input.is_action_pressed("right")):
 		#move right
-		velocity.x += 1
-	if(Input.is_action_pressed("left")):
+		velocity.x += SPEED
+	elif(Input.is_action_pressed("left")):
 		#move left
-		velocity.x += -1
-	velocity = velocity.normalized()*speed
+		velocity.x += -SPEED
+	else:
+		velocity.x = 0
 #	pass
 
 func _physics_process(delta):
 	get_input()
-	velocity = move_and_slide(velocity)
+	velocity.y += delta * G
+	velocity = move_and_slide(velocity,Vector2(0,-1))
